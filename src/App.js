477ecef557ff0966591dc5c13b3843572cef8a94
AnnaEmson <anna.emson@gmail.com>
2021-09-28 20:59:57 +0100

    add quiz component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
+import Quiz from './Quiz'
 import './App.css'
-
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
